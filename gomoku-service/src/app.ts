d279817bddbc44c48420e956811030e4274ba181
Hayden McCristal <hmccrist@myune.edu.au>
2023-09-26 16:16:03 +1000

    cleanup, added env to commits, renamed folder

--- gomoku-service/src/app.ts
+++ gomoku-service/src/app.ts
@@ -3,12 +3,9 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import testRouter from './router/test.router'
 import gamesRouter from './router/games.router'
 
 import connectDB from './util/connectDB';
-
-import { getAllGames } from './service/games.service';
 
 dotenv.config();
 
@@ -26,8 +23,6 @@ app.use(
     })
 )
 
-
-app.use('/test', testRouter);
 app.use('/games', gamesRouter);
 
 app.get('/', (req: Request, res: Response) => {
@@ -37,19 +32,3 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-
-/*
-
-app.get('/games', async (req: Request, res: Response) => {
-    try {
-        const games = await getAllGames();
-        return res.status(200).json({ games });
-    }
-    catch (e) {
-        console.log(e);
-        return res.status(500).send(e);
-    }
-    
-});
-
-*/
